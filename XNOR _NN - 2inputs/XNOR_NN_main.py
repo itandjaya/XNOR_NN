deae29cbdb35f8e755333640e0319affae1b08f0
wangtan83 <50927390+wangtan83@users.noreply.github.com>
2020-02-24 15:25:49 -0800

    Add plot J_Cost vs. Iterations

--- XNOR _NN - 2inputs/XNOR_NN_main.py
+++ XNOR _NN - 2inputs/XNOR_NN_main.py
@@ -2,8 +2,9 @@
 ## XNOR_NN_main.py
 
 import sys;
-from XNOR_NN import XNOR_NN;
-#import numpy as np;
+from XNOR_NN import XNOR_NN;        #   Neural Network XNOR class.
+import matplotlib.pyplot as plt     #   To plot Cost vs. # of iterations.
+
 
 def main():
 
@@ -19,15 +20,25 @@ def main():
                                     3,              ## 3 NN layers: Input, inter-layer, output.
                                     [2,2,1] );      ## num of nodes for each layer.
 
-    TEST1, TEST2 = True, True;
+    TEST1, TEST2    = True, True;
+    is_J_PLOT       =   False;
 
     if TEST1:
-        print("\nNeural Network XNOR - using GRADIENT DESCENT ITERATION\n", "#"*30, "\n");
-        
+        print("\nNeural Network XNOR - using GRADIENT DESCENT ITERATION\n", "#"*30, "\n");        
 
         xnor_nn.init_thetas();
         xnor_nn.train_NN();
         print("Final Cost J = ", xnor_nn.J_cost(xnor_nn.a[-1]));
+
+
+        if is_J_PLOT:
+            #   Plot the J Cost vs. # of iterations. J should coverge as iteration increases.
+            x_axis  =   range(xnor_nn.learning_iter);
+            y_axis  =   xnor_nn.J_cost_values;
+
+            plt.plot(   x_axis, y_axis, label='J_cost vs. # of Iterations');
+            plt.show();
+
 
         for test_input in X_inputs:
             test_res   =   xnor_nn.H_funct(    [test_input]    );
@@ -50,6 +61,4 @@ def main():
     return 0;
 
 if __name__ == '__main__':  main();
-
-
 
